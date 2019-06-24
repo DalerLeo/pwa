474ad6b83ae684805fad431c6c1e5bc75e97b554
SWNTG <swntg@SWNTGs-MacBook-Pro.local>
2019-06-25 00:26:20 +0500

    Updates

---
+++ precache-manifest.53f963716f3dd0ece533c11e9b9cfc8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0007015ef91cd3ba77083b73dd2065",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "aaff0e1ee4f879744280",
+    "url": "/pwa/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/pwa/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "aaff0e1ee4f879744280",
+    "url": "/pwa/static/js/main.837ec347.chunk.js"
+  },
+  {
+    "revision": "9326abcbfc116d8e730f",
+    "url": "/pwa/static/js/runtime~main.ff6970bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
