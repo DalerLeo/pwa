677b9207c3387531ee92718bdac39e723426eca1
SWNTG <swntg@SWNTGs-MacBook-Pro.local>
2019-06-26 07:42:22 +0500

    Updates

---
+++ precache-manifest.1d181e0ad32ef24cde647201e0e74610.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e953857a74e0ca447155d0f64c8c92c",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "ded66b86f21fe175e39c",
+    "url": "/pwa/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e97da577a06f05be360f",
+    "url": "/pwa/static/js/2.43996db3.chunk.js"
+  },
+  {
+    "revision": "ded66b86f21fe175e39c",
+    "url": "/pwa/static/js/main.87d6b157.chunk.js"
+  },
+  {
+    "revision": "9326abcbfc116d8e730f",
+    "url": "/pwa/static/js/runtime~main.ff6970bd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pwa/precache-manifest.7dc9fb784eb7562fa7090c99559587e9.js"
+  "/pwa/precache-manifest.1d181e0ad32ef24cde647201e0e74610.js"
 );
 
 self.addEventListener('message', (event) => {
