ece3aa3308e5f1613eb74c8780e66113ffeb1855
SWNTG <swntg@SWNTGs-MacBook-Pro.local>
2019-06-26 12:56:09 +0500

    Updates

--- precache-manifest.8d4aa3d4573aa502ed1e2cab3242b213.js
+++ precache-manifest.8d4aa3d4573aa502ed1e2cab3242b213.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "15c2bcd4fd0dc1711b813215bdc162a1",
+    "revision": "81cab8a66b0a9b1d587a56fb60264c1c",
     "url": "/pwa/index.html"
   },
   {
-    "revision": "1f0c60b1d677c3287e34",
+    "revision": "397a5495a6a74c021de4",
     "url": "/pwa/static/css/2.dc646829.chunk.css"
   },
   {
-    "revision": "f87da59e0626d4181a4e",
-    "url": "/pwa/static/css/main.32497562.chunk.css"
+    "revision": "cc64a3696e199bd2eb3d",
+    "url": "/pwa/static/css/main.8c48ae69.chunk.css"
   },
   {
-    "revision": "1f0c60b1d677c3287e34",
-    "url": "/pwa/static/js/2.633b8a4b.chunk.js"
+    "revision": "397a5495a6a74c021de4",
+    "url": "/pwa/static/js/2.8f87d6ce.chunk.js"
   },
   {
-    "revision": "f87da59e0626d4181a4e",
-    "url": "/pwa/static/js/main.b9a84a9b.chunk.js"
+    "revision": "cc64a3696e199bd2eb3d",
+    "url": "/pwa/static/js/main.0bbb9c48.chunk.js"
   },
   {
     "revision": "9326abcbfc116d8e730f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pwa/precache-manifest.1bb3fa463bd991aca0dbf7c59946e508.js"
+  "/pwa/precache-manifest.8d4aa3d4573aa502ed1e2cab3242b213.js"
 );
 
 self.addEventListener('message', (event) => {
